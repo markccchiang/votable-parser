482d4e9591389df0fe31f56c72955ec07c753caa
Mark C.C. Chiang <chcchiang@asiaa.sinica.edu.tw>
2020-01-03 19:02:54 +0800

    Add another catalog controller test

--- tests/TestVOTableController.cc
+++ tests/TestVOTableController.cc
@@ -4,17 +4,22 @@
 
 using namespace catalog;
 
+std::unique_ptr<Controller> _controller;
+
 void TestOnFilterRequest1();
 void TestOnFilterRequest2();
 void TestOnFilterRequest3();
 void TestOnFilterRequest4();
 void TestOnFilterRequest(OpenFileRequest open_file_request, FilterRequest filter_request);
+void TestOnFilterRequest2(OpenFileRequest open_file_request, FilterRequest filter_request);
 void TestOnOpenFileRequest();
 void TestOnOpenFileRequest(OpenFileRequest open_file_request);
 void TestOnFileListRequest();
 void TestOnFileListRequest(FileListRequest file_list_request);
 void TestOnFileInfoRequest();
 void TestOnFileInfoRequest(FileInfoRequest file_info_request);
+
+void TestMultiFiles();
 
 int main(int argc, char* argv[]) {
     // TestOnFileListRequest();
@@ -23,7 +28,8 @@ int main(int argc, char* argv[]) {
     // TestOnFilterRequest1();
     // TestOnFilterRequest2();
     // TestOnFilterRequest3();
-    TestOnFilterRequest4();
+    // TestOnFilterRequest4();
+    TestMultiFiles();
 
     return 0;
 }
@@ -162,7 +168,8 @@ void TestOnFilterRequest4() {
     filter_config1.max = 275.089;
     filter_request.filter_configs.push_back(filter_config1);
 
-    TestOnFilterRequest(open_file_request, filter_request);
+    // TestOnFilterRequest(open_file_request, filter_request);
+    TestOnFilterRequest2(open_file_request, filter_request);
 }
 
 void TestOnFilterRequest(OpenFileRequest open_file_request, FilterRequest filter_request) {
@@ -183,4 +190,108 @@ void TestOnFilterRequest(OpenFileRequest open_file_request, FilterRequest filter
     CloseFileRequest close_file_request;
     close_file_request.file_id = open_file_request.file_id;
     controller.OnCloseFileRequest(close_file_request);
+}
+
+void TestOnFilterRequest2(OpenFileRequest open_file_request, FilterRequest filter_request) {
+    // Open file
+    OpenFileResponse open_file_response;
+    _controller = std::unique_ptr<Controller>(new Controller());
+    _controller->OnOpenFileRequest(open_file_request, open_file_response);
+
+    // Filter the file data
+    _controller->OnFilterRequest(filter_request, [&](FilterResponse filter_response) {
+        // Print partial or final results
+        filter_request.Print();
+        filter_response.Print();
+        std::cout << "\n------------------------------------------------------------------\n";
+    });
+
+    // Close file
+    CloseFileRequest close_file_request;
+    close_file_request.file_id = open_file_request.file_id;
+    _controller->OnCloseFileRequest(close_file_request);
+
+    // Delete the Controller
+    _controller.reset();
+}
+
+void TestMultiFiles() {
+    // First file messages
+    OpenFileRequest open_file_request1;
+    open_file_request1.directory = "images";
+    open_file_request1.filename = "simple.xml";
+    open_file_request1.file_id = 0;
+    open_file_request1.preview_data_size = 0;
+
+    FilterRequest filter_request1;
+    filter_request1.file_id = 0;
+    filter_request1.subset_start_index = 0;
+    filter_request1.subset_data_size = 50;
+    filter_request1.region_id = 0;
+    filter_request1.image_bounds = {-1, -1, -1, -1};
+    filter_request1.hided_table_headers = {"Name", "RVel", "e_RVel", "R"};
+
+    FilterConfig filter_config1;
+    filter_config1.column_name = "RA";
+    filter_config1.comparison_operator = FromTo;
+    filter_config1.min = 0;
+    filter_config1.max = 100;
+    filter_request1.filter_configs.push_back(filter_config1);
+
+    // Second file messages
+    OpenFileRequest open_file_request2;
+    open_file_request2.directory = "images";
+    open_file_request2.filename = "M17_SWex_simbad_2arcmin.xml";
+    open_file_request2.file_id = 1;
+    open_file_request2.preview_data_size = 0;
+
+    FilterRequest filter_request2;
+    filter_request2.file_id = 1;
+    filter_request2.subset_start_index = 0;
+    filter_request2.subset_data_size = 10;
+    filter_request2.region_id = 0;
+    filter_request2.image_bounds = {-1, -1, -1, -1};
+    filter_request2.hided_table_headers = {"OID4", "XMM:Obsno", "IUE:bibcode", "IUE:F", "IUE:Comments", "IUE:S", "IUE:CEB", "IUE:m",
+        "IUE:ExpTim", "IUE:Time", "IUE:ObsDate", "IUE:MD", "IUE:FES", "IUE:A", "IUE:IMAGE"};
+
+    FilterConfig filter_config2;
+    filter_config2.column_name = "RA_d";
+    filter_config2.comparison_operator = GreaterThan;
+    filter_config2.min = 275.089;
+    filter_config2.max = 275.089;
+    filter_request2.filter_configs.push_back(filter_config2);
+
+    // Create the Controller
+    _controller = std::unique_ptr<Controller>(new Controller());
+
+    // Open files
+    OpenFileResponse open_file_response1;
+    OpenFileResponse open_file_response2;
+    _controller->OnOpenFileRequest(open_file_request1, open_file_response1);
+    _controller->OnOpenFileRequest(open_file_request2, open_file_response2);
+
+    // Filter the first file
+    _controller->OnFilterRequest(filter_request1, [&](FilterResponse filter_response) {
+        // Print partial or final results
+        filter_request1.Print();
+        filter_response.Print();
+        std::cout << "\n------------------------------------------------------------------\n";
+    });
+
+    // Filter the second file
+    _controller->OnFilterRequest(filter_request2, [&](FilterResponse filter_response) {
+        // Print partial or final results
+        filter_request1.Print();
+        filter_response.Print();
+        std::cout << "\n------------------------------------------------------------------\n";
+    });
+
+    // Close files
+    CloseFileRequest close_file_request1;
+    close_file_request1.file_id = open_file_request1.file_id;
+    _controller->OnCloseFileRequest(close_file_request1);
+
+    CloseFileRequest close_file_request2;
+    close_file_request2.file_id = open_file_request2.file_id;
+    _controller->OnCloseFileRequest(close_file_request2);
 }
